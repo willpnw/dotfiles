2e35c9a67b748d4a3ba6734627b28743840e79ea
Will Patterson <wpatterson@witekio.com>
2021-02-07 13:54:43 -0800

    check

--- configs/waybar/modules/ticker.sh
+++ configs/waybar/modules/ticker.sh
@@ -1,89 +1,36 @@
 #!/bin/bash
 
-set -e
+SYMBOLS=(LMND  NIO   PLUG  ZOM)
+LIMITS=( 155.8 58.47 68.45 1.96)
 
-LANG=C
-LC_NUMERIC=C
-
-TARGET=203
-SYMBOLS=APPN
-#SYMBOLS=("$@")
-
-if ! $(type jq > /dev/null 2>&1); then
-  echo "'jq' is not in the PATH. (See: https://stedolan.github.io/jq/)"
-  exit 1
+INDEX_FILE=/tmp/index
+if [ ! -f $INDEX_FILE ]; then
+    echo 0 > $INDEX_FILE
 fi
 
-if [ -z "$SYMBOLS" ]; then
-  echo "Usage: ./ticker.sh AAPL MSFT GOOG BTC-USD"
-  exit
-fi
+INDEX=$(cat $INDEX_FILE)
+echo $(((INDEX+1) % ${#SYMBOLS[@]})) > $INDEX_FILE
 
-FIELDS=(symbol marketState regularMarketPrice regularMarketChange regularMarketChangePercent \
-  preMarketPrice preMarketChange preMarketChangePercent postMarketPrice postMarketChange postMarketChangePercent)
+SYMBOL=${SYMBOLS[$INDEX]}
+LIMIT=${LIMITS[$INDEX]}
+FIELDS=regularMarketPrice,regularMarketChange,regularMarketChangePercent
 API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
 
-if [ -z "$NO_COLOR" ]; then
-  : "${COLOR_BOLD:=\e[1;37m}"
-  : "${COLOR_GREEN:=\e[32m}"
-  : "${COLOR_RED:=\e[31m}"
-  : "${COLOR_RESET:=\e[00m}"
-fi
+results=$(curl --silent "$API_ENDPOINT&fields=$FIELDS&symbols=$SYMBOL" | jq '.quoteResponse .result')
 
-symbols=$(IFS=,; echo "${SYMBOLS[*]}")
-fields=$(IFS=,; echo "${FIELDS[*]}")
-
-results=$(curl --silent "$API_ENDPOINT&fields=$fields&symbols=$symbols" \
-  | jq '.quoteResponse .result')
-
-query () {
-  echo $results | jq -r ".[] | select(.symbol == \"$1\") | .$2"
+getValue() {
+    printf "%.2f" $(echo $results | jq .[0].$1)
 }
 
-for symbol in $(IFS=' '; echo "${SYMBOLS[*]}" | tr '[:lower:]' '[:upper:]'); do
-  marketState="$(query $symbol 'marketState')"
+price=$(getValue regularMarketPrice)
+change=$(getValue regularMarketChange)
+percent=$(getValue regularMarketChangePercent)
 
-  if [ -z $marketState ]; then
-    printf 'No results for symbol "%s"\n' $symbol
-    continue
-  fi
+if echo $change | grep -q -; then
+    class="stock-down"
+else
+    class="stock-up"
+fi
 
-  preMarketChange="$(query $symbol 'preMarketChange')"
-  postMarketChange="$(query $symbol 'postMarketChange')"
-
-  if [ $marketState == "PRE" ] \
-    && [ $preMarketChange != "0" ] \
-    && [ $preMarketChange != "null" ]; then
-    nonRegularMarketSign='*'
-    price=$(query $symbol 'preMarketPrice')
-    diff=$preMarketChange
-    percent=$(query $symbol 'preMarketChangePercent')
-  elif [ $marketState != "REGULAR" ] \
-    && [ $postMarketChange != "0" ] \
-    && [ $postMarketChange != "null" ]; then
-    nonRegularMarketSign='*'
-    price=$(query $symbol 'postMarketPrice')
-    diff=$postMarketChange
-    percent=$(query $symbol 'postMarketChangePercent')
-  else
-    nonRegularMarketSign=''
-    price=$(query $symbol 'regularMarketPrice')
-    diff=$(query $symbol 'regularMarketChange')
-    percent=$(query $symbol 'regularMarketChangePercent')
-  fi
-
-  if [ "$diff" == "0" ]; then
-    color=
-  elif ( echo "$diff" | grep -q ^- ); then
-    color=$COLOR_RED
-  else
-    color=$COLOR_GREEN
-  fi
-
-#  if [ "$price" != "null" ]; then
-#    printf "%-10s$COLOR_BOLD%8.2f$COLOR_RESET" $symbol $price
-#    printf "$color%10.2f%12s$COLOR_RESET" $diff $(printf "(%.2f%%)" $percent)
-#    printf " %s\n" "$nonRegularMarketSign"
-#  fi
-    echo -e "{\"text\": \"$SYMBOLS: [$TARGET] $price\"}"
-done
+text="$SYMBOL $LIMIT/$price $change ($percent %)"
+echo -e "{\"text\": \"$text\", \"class\": \"$class\"}"
